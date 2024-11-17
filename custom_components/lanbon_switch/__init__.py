689b1b1fc9e5c1e33198c384c3aa6208da486226
Francisco <francisco.cabrera@builder.ai>
2024-11-17 22:34:40 +0000

    Small updates

--- custom_components/lanbon_switch/__init__.py
+++ custom_components/lanbon_switch/__init__.py
@@ -91,10 +91,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
         # Forward setup only if not already done
         if not hass.data[DOMAIN]["platform_switch_setup_done"]:
-            hass.data[DOMAIN]["platform_switch_setup_done"] = True
             hass.async_create_task(
                 hass.config_entries.async_forward_entry_setup(entry, "switch")
             )
+            hass.data[DOMAIN]["platform_switch_setup_done"] = True
 
     # Request device states on startup
     async def sync_device_states(event):
